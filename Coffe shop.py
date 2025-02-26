36f7292df48fe2e7f348c9197b88340c6f3064f3
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-26 20:15:53 +0600

    Update Coffe shop.py

--- Coffe shop.py
+++ Coffe shop.py
@@ -30,7 +30,7 @@ if another_order =="Yes":
     item_2 = input("Enter the name of second item= ")
     if item_2 in menu:
        order_total += menu[item_2]
-       print(f"Item {item_2} haas been added to order")
+       print(f"Item {item_2} has been added to order")
     else:
         print(f"Ordered item {item_2}is not available")
 
